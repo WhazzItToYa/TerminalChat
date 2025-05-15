1489bb1d9decaa73fda29feea52d1ac38a6533c5
WhazzItToYa <whazzittoya@gmail.com>
2025-05-15 12:14:39 -0700

    use parts instead of emote substitution.

--- terminalchat.js
+++ terminalchat.js
@@ -157,63 +157,24 @@ function scrollOff(numLines)
 
 // Creates the html for a message containing emotes.
 function insertEmotes(messageData) {
+    console.log(`Message is ${JSON.stringify(messageData), null, 2}`);
+    
     let textMessage = messageData.message.message;
-    let emotes = messageData.emotes;
-
-    // Make sure the emotes are sorted left to right
-    emotes.sort((e1, e2) => e1.startIndex - e2.startIndex);
-
-    // We'll go through, span by span (span=substrings on emote
-    // boundaries) extracting the span from the source message, and
-    // appending or discarding&substituting the emote code.  The thing
-    // that makes it tricky is that the emote boundary indexes count
-    // UTF-32 characters (encoded as 2 javascript characters) as a
-    // single char, so substrings & indexes need to count those chars
-    // correctly.
 
     let outputMessage = "";
-    let inputPos = 0;
-    let prevIndex = 0;
-    for (const emote of emotes)
+    for (const part of messageData.parts)
     {
-        // Get the portion of the message before the emote.
-        let span = substring(textMessage, inputPos, emote.startIndex-prevIndex);
-        outputMessage += span;
-        inputPos += span.length;
-        prevIndex = emote.startIndex;
-        
-        console.log(`emote ${emote.name} : ${emote.startIndex}-${emote.endIndex}`);
-        console.log(`text is ${span}`);
-
-        // Now get the portion of the message corresponding to the emote.
-        let emoteSpan = substring(textMessage, inputPos, emote.endIndex+1-prevIndex);
-        outputMessage += `<img src="${encodeURI(emote.imageUrl)}">`;
-        inputPos += emoteSpan.length;
-        prevIndex = emote.endIndex+1;
-    }
-    // And then the remaining portion of the message.
-    outputMessage += textMessage.substring(inputPos);
-    return outputMessage;
-}
-
-// Returns the substring of SOURCESTRING beginning at STARTINDEX position
-// which contains SUBSTRINGLENGTH unicode characters (including > 0xffff).
-
-function substring(sourceString, startIndex, substringLength)
-{
-    let endIndex = startIndex;
-    for (var i = 0; i < substringLength; i++)
-    {
-        let codeUnit = sourceString.charCodeAt(endIndex);
-        if (codeUnit >= 0xD800 && codeUnit <= 0xDBFF)
+        console.log(`processing ${JSON.stringify(part)}`);
+        if (part.type === "text")
         {
-            endIndex += 2;
-        } else
+            outputMessage += part.text;
+        }
+        else if (part.type === "emote")
         {
-            endIndex += 1;
+            outputMessage += `<img src="${encodeURI(part.imageUrl)}">`;
         }
     }
-    return sourceString.substring(startIndex, endIndex);
+    return outputMessage;
 }
 
 // Removes all messages from a particular USERID from the display.
